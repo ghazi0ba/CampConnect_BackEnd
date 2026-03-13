fe3f6ea377eac688be33b4c8b77ce8329d802659
ghazi <ghazi.benamara@sofrecom.com>
2026-03-14 00:25:12 +0100

    first commit

---
+++ src/main/java/com/example/campconnect_backend/CampConnectBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.campconnect_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampConnectBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampConnectBackEndApplication.class, args);
+    }
+
+}
